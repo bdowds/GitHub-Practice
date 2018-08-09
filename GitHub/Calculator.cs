58001c193a84429adfa778d50725df13cbfdb639
bdowds <bwdowds@gmail.com>
2018-08-09 12:15:03 -0500

    Initial commit, Stubbed out methods

---
+++ GitHub/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitHub
+{
+    class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return 0;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return 0;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return 0;
+        }
+
+        public double Divide(int a, int b)
+        {
+            return 0;
+        }
+    }
+}
